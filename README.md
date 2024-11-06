678166cdb975fac237382972ae60ad532f2f293d
Baptiste Pillot <baptiste@pillot.fr>
2024-11-06 17:30:37 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,14 @@
+# build
+
+Permanently apply JavaScript modifiers to your dynamic DOM.
+
+## Demo, Testing and Development
+
+```bash
+git clone https://github.com/itrocks-ts/build
+cd build
+npm install
+npm run build
+```
+
+To test, serve **build.html** from a local web server and open your browser's console to see it in action.
---
+++ demo/build.ts
@@ -0,0 +1,15 @@
+import build from '../src/build.js'
+
+build('main article #div #active', active => active.setAttribute('title', 'Title dynamically added'))
+
+build({ selector: '#active', name: 'click', callback: (event: Event) => console.log('click #active', event) })
+
+build('div.a', 'click', event => console.log('click .a', event))
+
+build(element => console.log('new element', element))
+
+document.getElementById('change')?.addEventListener('click', () => {
+	const div = document.getElementById('div')
+	if (!div) return
+	div.innerHTML = '<div><div id="active">This is active content</div></div><div class="a">Another one</div>'
+})
